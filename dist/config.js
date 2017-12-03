fc0384a8964a7e5909c120f3be1a9d5d4d2b01e9
Will Pearson <will@mcneel.com>
2017-12-03 10:59:47 +0000

    Initial commit

---
+++ dist/config.js
@@ -0,0 +1,10 @@
+var SpkAppConfig = {
+  serverUrl: 'http://immense-wildwood-92051.herokuapp.com/api',
+  allowGuestAccess: true,
+  // serverUrl: 'https://server.speckle.works',
+  // logoUrl: 'dist/logo.png'
+  logoUrl: 'https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-04-04/164468419442_9561f3ab8bcd823ebe33_132.png'
+  // logoUrl: 'http://formatengineers.com/images/format-logo.png'
+}
+
+window.SpkAppConfig = SpkAppConfig
